476bbdc91dfa535ebef95980d8ed83f9b5beb8cc
Fredrik Skaug <fredrik@dhcp-10-22-4-237.wlan.ntnu.no>
2019-11-06 13:27:29 +0100

    oppdatert_about

---
+++ script/main.js
@@ -0,0 +1,14 @@
+//about
+function openContactform(){
+    document.getElementById("contactForm").style.display = "flex";
+    document.getElementById("contactButton").style.display = "none";
+    document.getElementById("contactWrapper").style.width = "610px";
+    document.getElementById("contactWrapper").style.height = "710px";
+}
+
+function closeContactform() {
+    document.getElementById("contactForm").style.display = "none";
+    document.getElementById("contactButton").style.display = "flex";
+    document.getElementById("contactWrapper").style.width = "215px";
+    document.getElementById("contactWrapper").style.height = "60px";
+}
