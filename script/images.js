7b862090db905bf75c8bab5b74e74fcb0c8d4135
Sigbjørn Berdal <sigbjorn.berdal14@gmail.com>
2019-11-08 17:16:18 +0100

    skrtskrt

--- script/images.js
+++ script/images.js
@@ -1,16 +1,34 @@
 var slideIndex = 0;
-carousel();
 let time;
-time = setInterval(carousel, 5000); // Change image every 5 seconds
+time = setInterval(carousel, 5000); // Endre bildet hvert 5 sekund
+carousel();
 function carousel() {
     var i;
-    var x = document.getElementsByClassName("mySlides");
+    var x = document.getElementsByClassName("slides");
     for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
+        x[i].style.display = "none";
     }
     slideIndex++;
     if (slideIndex > x.length) {slideIndex = 1}
-    x[slideIndex-1].style.display = "block";
+        x[slideIndex-1].style.display = "block";
+}
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("slides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
 }
 
 let playPause = document.getElementById("playPause");
@@ -24,15 +42,20 @@ imgSlideShow.addEventListener("mouseleave", () => {
     playPause.style.display = "none";
 });
 let isPlaying = true;
+const navButtons = document.querySelectorAll(".imageSlideShowBtn");
+navButtons[0].style.display = "flex !important";
 function playPauseFunction () {
     if (isPlaying) {
         this.src="./media/playMusic.png"
         clearInterval(time);
         isPlaying = false;
+        navButtons[0].style.display = "flex";
+        navButtons[1].style.display = "flex";
     } else {
         this.src="./media/pauseMusic.png"
         time = setInterval(carousel, 5000);
         isPlaying = true;
+        navButtons[0].style.display = "none";
+        navButtons[1].style.display = "none";
     }
-    // console.log(isPlaying);
 }
