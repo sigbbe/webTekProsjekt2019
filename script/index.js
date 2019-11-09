7bdd48a0ad322c40641aa415c766177b465b6341
Sigbjørn Berdal <sigbjorn.berdal14@gmail.com>
2019-11-09 16:35:51 +0100

    suicide

---
+++ script/index.js
@@ -0,0 +1,8 @@
+let animationDelay = setTimeout(animationDelayFunction, 2000);
+
+
+
+function animationDelayFunction () {
+    console.log(document.getElementById("wallpaper").childNodes);
+    
+}
